3ca062ee1998a728b26fafb15d1055be9d8ff828
Edison Galeano Ramirez <edisongaleano007@hotmail.com>
2022-10-10 23:52:42 -0500

    boton crear

--- sistema/sistema/libreria/views.py
+++ sistema/sistema/libreria/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#acceso el sitio
 def inicio(request):
     return render(request, 'paginas/inicio.html')
-   # Create your views here.
-
+   
 def nosotros(request):
     return render(request,'paginas/nosotros.html')
 
+      #acceso a libros
+
 def libros(request):
     return render(request, "libros/index.html")    
+
+def crear(request):
+    return render(request, "libros/crear.html")    
