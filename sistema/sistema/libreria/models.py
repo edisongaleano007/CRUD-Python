657986a83bb3b3cee23cb8ea531e54324a6ae415
Edison Galeano Ramirez <edisongaleano007@hotmail.com>
2022-10-11 23:04:32 -0500

    db y migracion

--- sistema/sistema/libreria/models.py
+++ sistema/sistema/libreria/models.py
@@ -1,3 +1,9 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    imagen = models.ImageField(upload_to='imagenes/',verbose_name='Imagen', null=True)
+    descripcion = models.TextField(verbose_name='Descripcion' ,null=True)
